0b673a1f9145ae319d58c9ec6dde3f2f99bce87c
754356226 <754356226@qq.com>
2021-01-13 15:47:17 +0800

    Update BiliMain.java

--- src/main/java/top/misec/BiliMain.java
+++ src/main/java/top/misec/BiliMain.java
@@ -26,11 +26,12 @@ public class BiliMain {
         logger.info(args[1]);
         logger.info(args[2]);
         logger.info(args[3]);
-        Verify.verifyInit(args[0], args[1], args[2], args[3]);
+//         Verify.verifyInit(args[0], args[1], args[2], args[3]);
 
-        if (args.length > 4) {
-            ServerVerify.verifyInit(args[4]);
-        }
+//         if (args.length > 4) {
+//             ServerVerify.verifyInit(args[4]);
+//         }
+        Verify.verifyInit("437218043", "e3c8425c%2C1620522526%2C1016d*b1", "0095068659361a624843c0a9935c993a", "73E4E439-EA0E-4413-ABBF-393BE3194E01143099infoc");
 
 
         //每日任务65经验
